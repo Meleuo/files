0507e7c9982019b5aa190fe588e17ca466481dbb
Meleuo <48749322+Meleuo@users.noreply.github.com>
2020-11-27 11:33:54 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "修改indigo配置文件"
+curl https://raw.githubusercontent.com/Meleuo/files/master/_config_indigo.yml  > /app/blog/themes/indigo/_config.yml
+
+echo "修改hexo配置文件"
+curl https://raw.githubusercontent.com/Meleuo/files/master/_config.yml > /app/blog/_config.yml
