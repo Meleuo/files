26bbebfe39cf4f6691bb22ea563c987584ad30f9
Meleuo <48749322+Meleuo@users.noreply.github.com>
2020-11-27 15:17:25 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,3 @@ curl https://raw.githubusercontent.com/Meleuo/files/master/_config_indigo.yml  >
 
 echo "修改hexo配置文件"
 curl https://raw.githubusercontent.com/Meleuo/files/master/_config.yml > /app/blog/_config.yml
-
-echo "修改语法兼容问题"
-curl https://raw.githubusercontent.com/Meleuo/files/master/lexer.js > /app/blog/node_modules/nunjucks/src/lexer.js
