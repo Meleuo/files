9fbed98f899c4084c23e04544c82f364147c6997
Meleuo <48749322+Meleuo@users.noreply.github.com>
2021-07-19 16:20:35 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,3 +5,6 @@ curl https://raw.githubusercontent.com/Meleuo/files/master/_config_indigo.yml  >
 
 echo "修改hexo配置文件"
 curl https://raw.githubusercontent.com/Meleuo/files/master/_config.yml > /app/blog/_config.yml
+
+echo "UPDATE CNAME"
+curl https://raw.githubusercontent.com/Meleuo/files/master/CNAME > /app/blog/source/CNAME
